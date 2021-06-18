96508b33369f0914cb182a0222761a827f264c79
schueler <schueler@ubuntu>
2021-06-18 11:07:41 +0200

    init myTeam

---
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -0,0 +1,11 @@
+package pkgMain;
+
+import pkgData.Player;
+
+public class MainAppl {
+	public static void main(String[] args) {
+		
+		Player player=new Player(2,"Dragovic","Defence");
+		System.out.println("=="+player);
+	}
+}
