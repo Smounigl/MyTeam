fe01bdca1634d753acc33df3978355324b55b135
schueler <schueler@ubuntu>
2021-06-21 15:34:20 +0200

    test

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -7,8 +7,8 @@ public class MainAppl {
 		
 		Player player=new Player(2,"Dragovic","Defence");
 		System.out.println("=="+player);
-		Player player=new Player(4,"Hinteregger","Defence");
+		player=new Player(4,"Hinteregger","Defence");
 		System.out.println("=="+player);
-		
+		System.out.println("TEST");
 	}
 }
